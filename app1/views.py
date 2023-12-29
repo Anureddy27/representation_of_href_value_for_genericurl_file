b11b908d5e4e0b5d077b199c4b60f48a47afef40
Anureddy27 <bhumireddyanusha143@gmail.com>
2023-12-29 09:05:23 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Anu(request):
+    return render(request,'Anu.html',)
+
+def babu(request):
+    return render(request,'babu.html',)
